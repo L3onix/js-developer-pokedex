ae8453b32cf9a2fb858d1edde78ae763c14841c8
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2023-12-05 04:00:27 -0300

    implementação de estrutura base para tela de detalhes do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}"><span hidden>${pokemon.number}</span>
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -27,6 +27,10 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+        const elements = document.getElementsByClassName('pokemon')
+        for(let x = 0; x < elements.length; x++) {
+            elements[x].addEventListener('click', onClickPokemon)
+        }
     })
 }
 
@@ -45,3 +49,65 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function onClickPokemon(event) {
+    const id = event.currentTarget.firstChild.innerHTML
+    loadPokemonDetails(id)
+}
+
+function loadPokemonDetails(id) {
+    pokeApi.getPokemonById(id).then((pokemon) => {
+        // console.log(pokemon)
+        const sectionPokemonDetail = document.getElementById('pokemon-detail-section')
+        const sectionPokemonList = document.getElementById("pokemon-list-section")
+        sectionPokemonDetail.innerHTML += constructPokemonDetailHtml(pokemon)
+        sectionPokemonDetail.classList.add(pokemon.types[0].type.name)
+        sectionPokemonDetail.removeAttribute("hidden")
+        sectionPokemonList.setAttribute("hidden", true)
+    })
+}
+
+function constructPokemonDetailHtml(pokemon) {
+    return `
+        <h1 class="name">${pokemon.name}</h1>
+        <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type.type.name}">${type.type.name}</li>`).join('')}
+        </ol>
+        <img src="${pokemon.sprites.other.dream_world.front_default}"/>
+        <div class="info">
+            <div>
+                <ol><li>abas</li></ol>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <td>Species</td> <td>asdfasdf</td>
+                    </tr>
+                    <tr>
+                        <td>Height</td> <td>${pokemon.height}</td>
+                    </tr>
+                    <tr>
+                        <td>Weight</td> <td>${pokemon.weight}</td>
+                    </tr>
+                    <tr>
+                        <td>Abilities</td> <td>asdfasdf</td>
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <h3>Breeding</h3>
+                <table>
+                    <tr>
+                        <td>Gender</td> <td>asdfasdf</td>
+                    </tr>
+                    <tr>
+                        <td>Egg groups</td> <td>asdfasdf</td>
+                    </tr>
+                    <tr>
+                        <td>Egg cycle</td> <td>asdfasdf</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    `;
+}
